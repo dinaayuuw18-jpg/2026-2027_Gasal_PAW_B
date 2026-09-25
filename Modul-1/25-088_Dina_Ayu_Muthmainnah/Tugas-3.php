8d6c98175aa90934401282217f63899af2cfdc6f
dinaayuuw18-jpg <dinaayuuw18@gmail.com>
2026-09-25 14:17:22 +0700

    Mengerjakan Modul 1

--- Modul-1/25-088_Dina_Ayu_Muthmainnah/Tugas-3.php
+++ Modul-1/25-088_Dina_Ayu_Muthmainnah/Tugas-3.php
@@ -1,1 +1,17 @@
-Tugas-3.php
+<?php 
+//ini non-embeded script
+echo "Hello Word"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php 
+	//ini embeded-script
+	echo "Hello World"; ?>
+</body>
+</html>
